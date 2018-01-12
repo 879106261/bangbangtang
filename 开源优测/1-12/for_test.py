339b29a87ca942aa7c01c0e7b889fc03c2962a08
879106261 <879106261@qq.com>
2018-01-12 19:42:32 +0800

    1-11学习总结

--- 开源优测/1-12/for_test.py
+++ 开源优测/1-12/for_test.py
@@ -3,17 +3,19 @@ for i in range(5):
     print(i,end=",")
 #换行
 print('')
-
-#打印99乘法表
 for i in range(0,10,2):
     print(i,end=',')
 print('')
 
+#打印99乘法表
 for i in range(1,10):
     for j in range(i,10):
         print('%d*%d=%d'%(i,j,i*j),end=' ')
     print('')
+print('')
 
-
+#打印三角形
 for i in range(1,10):
     print('*'*i,end='\n')
+
+
